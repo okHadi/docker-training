1a050885c8a0234d6d5fe94c71c2fd8a48336240
Hadi Khan <hadiabdullahshahab@gmail.com>
2022-08-04 20:41:51 +0500

    CNode app created

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send("Hello world")
+})
+
+app.listen(8081,()=>{
+    console.log("App is running on the port 8081")
+})
